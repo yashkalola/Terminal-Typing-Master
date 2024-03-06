49b14a436273f435389e652d384feaede0ec06f2
yashkalola <yashkalola2110@gmail.com>
2024-03-06 09:53:38 +0530

    typing test

--- TypingMaster.py
+++ TypingMaster.py
@@ -16,3 +16,27 @@ class TypingMaster:
         self.current_user = None
         self.results = []
         self.exit_flag = False
+
+    def get_username(self):
+        user_name = input("Enter your name: ")
+        self.current_user = user_name
+
+    def start_typing_test(self):
+        input("Press Enter to start typing test...")
+        print("\nTyping test starts now. Type the following words:\n")
+
+        random_words = random.sample(self.words, len(self.words))
+        words_to_type = " ".join(random_words)
+        print(words_to_type)
+
+        start_time = time.time()
+
+        typed_words = input("\nType the words: ")
+
+        while " ".join(typed_words.split()) != words_to_type:
+            print("Incorrect! Please retype the words.")
+            start_time = time.time()  # Reset the timer
+            typed_words = input("Type the words again: ")
+
+        end_time = time.time()
+        self.calculate_wpm(start_time, end_time, words_to_type, typed_words)
